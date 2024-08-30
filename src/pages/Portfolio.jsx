960aa3e1978e444ac5871ba73e716193d314be6e
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-08-30 15:52:54 +0100

    feat update services

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Projects from '../components/Projects'
 import Footer from '../components/Footer'
+import Impact from '../components/Impact'
 
 const Portfolio = () => {
   return (
     <div className='mt-24'>
       <Navbar/>
-      <h2 className="text-3xl text-secondary  text-center font-bold">Courses</h2>
-      <p className='text-secondary text-sm p-4 text-center w-fit mx-auto'>We prepare your child to excel in different courses</p>
+      <h2 className="text-3xl text-primary  text-center font-bold">Courses</h2>
+      <p className='text-brand text-sm p-4 text-center w-fit mx-auto'>We provide quality and affordable learning to help your child thrive.</p>
 
       {/* <Projects/> */}
+      <Impact/>
       <Footer/>
     </div>
   )
