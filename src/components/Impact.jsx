b3b184145d6d445625b74c16ed931b362795f191
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-08-30 15:49:49 +0100

    feat: add exams

--- src/components/Impact.jsx
+++ src/components/Impact.jsx
@@ -4,12 +4,12 @@ const Impact = () => {
   return (
     <div className='flex text-brand flex-col md:flex-row items-center p-4 gap-4'>
         <div className="flex border rounded-lg border-secondary shadow flex-col items-center w-[90%] md:w-[30%] ">
-            <h3 className="text-3xl font-bold">GCSE</h3>
-            <p className="text-xl">EDEXCEL, AQA, OCR etc.</p>
+            <h3 className="text-3xl font-bold">K-12 Academics</h3>
+            <p className="text-lg">GCSE, IGCSE, AQA, OCR etc.</p>
         </div>
         <div className="flex border rounded-lg border-secondary shadow flex-col items-center w-[90%] md:w-[30%] ">
-            <h3 className="text-3xl font-bold">IGCSE</h3>
-            <p className="text-xl"></p>
+            <h3 className="text-3xl font-bold">Tests & Exams</h3>
+            <p className="text-lg font-normal">IELTS, SATs, etc.</p>
         </div>
     </div>
   )
