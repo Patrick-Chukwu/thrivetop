6ab57fd32a706bafcf76e9070a998f4c6daf5b15
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-09-04 10:19:50 +0100

    fix: edt map

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const GoogleMap = () => {
   return (
-    <div className="flex justify-center items-center p-4">
+    <div className="flex justify-center items-center">
       <iframe
         title="Thrivetop Academy Location"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1515128.5414564447!2d3.3690825482445814!3d8.090728213161483!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10398d87b8ace9d7%3A0x88b18963269f6904!2sThrivetop%20Academy!5e0!3m2!1sen!2sng!4v1725441248116!5m2!1sen!2sng"
