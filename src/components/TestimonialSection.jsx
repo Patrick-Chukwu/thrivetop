cf8837129dc4bb72b2cd8f861bc69fa9928f3b65
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-09-07 11:08:48 +0100

    feat: update the design

--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -7,7 +7,6 @@ import four from '../assets/AbisolaChemi.webp'
 import five from '../assets/Aramide_English.webp'
 
 
-// Sample data for testimonials
 const testimonials = [
   {
     id: 1,
@@ -69,8 +68,8 @@ const TestimonialSection = () => {
       }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center py-10 bg-primary gap-6">
-        <h2 className="text-2xl md:text-3xl font-bold text-white text-center w-full capitalize">Meet our tutors</h2>
+    <div className="flex flex-col items-center justify-center pb-10 py-4 bg-primary gap-6">
+       
         <div className="flex justify-between items-center gap-2 mt-2"> 
         <a href="https://docs.google.com/forms/d/e/1FAIpQLSffQMfu-pAJapvGIeIMW4VULmAUWySDN57C_3qp6VS8lu40hg/viewform?usp=sf_link" target='_blank' className='text-white text-base text-center bg-secondary px-4 py-2 rounded-md hover:bg-white hover:text-bcolor font-medium '>Become a Tutor</a>
         <p className="text-sm md:text-xl text-center text-white ">Join a community of top tutors.</p>
@@ -84,9 +83,9 @@ const TestimonialSection = () => {
         {testimonials.map((testimonial) => (
           <div
             key={testimonial.id}
-            className="flex-shrink-0 w-full md:w-1/3 p-4"
+            className="flex-shrink-0 w-full md:w-1/2 p-4"
           >
-            <div className="bg-white rounded-lg shadow-lg p-4 py-2 min-h-[520px] flex flex-col items-center">
+            <div className="bg-white rounded-lg shadow-lg p-4 py-2 h-[520px] md:min-h-[450px] flex flex-col items-center">
               <img
                 className="size-40 rounded-full mb-2 md:mb-4"
                 src={testimonial.image}
