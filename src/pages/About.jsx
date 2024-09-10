79c8cfc37a97e0a0b9b301d342d0882599f5642c
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-09-10 15:31:43 +0100

    feat: update about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,7 @@ import Footer from '../components/Footer'
 import mission from  '../assets/mission.svg'
 import vision from  '../assets/vision.svg'
 import Impact from '../components/Impact'
+import Tutors from '../components/Tutors'
 
 const About = () => {
   return (
@@ -28,7 +29,7 @@ const About = () => {
           {/* first */}
     <div className="gap-4 flex flex-col justify-center items-center text-center sm:p-8 py-4 border border-primary hover:bg-primary size-[350px] sm:w-[500px] sm:h-[500x] rounded-2xl shadow-lg">
       <img className='size-12' src={mission} alt="vision icon" />
-      <h4 className="font-semibold text-3xl text-brand hover:text-white">Mission</h4>
+      <h4 className="font-semibold text-2xl text-brand hover:text-white">Mission</h4>
       <p className="text-xl font-light px-2">To foster a nurturing and dynamic learning environment that cultivates intellectual growth, creativity, and global citizenship. 
       </p>
     </div>
@@ -36,13 +37,16 @@ const About = () => {
     {/* second */}
     <div className="gap-4 flex flex-col justify-center items-center text-brand text-center sm:p-8 py-4 border border-primary hover:bg-primary size-[350px]  sm:w-[500px] sm:h-[500x] rounded-2xl shadow-xl">
       <img className='size-12' src={vision} alt="vision icon" />
-      <h4 className="font-semibold text-3xl hover:text-white">Vision</h4>
+      <h4 className="font-semibold text-2xl hover:text-white">Vision</h4>
       <p className=" text-xl font-light px-2 sm:p-4">To be a global leader in innovative education, empowering students to excel academically, personally, and professionally.</p>
     </div>
         </div>
       </div>
+      <h2 className="text-2xl md:text-3xl font-bold text-primary text-center w-full p-4 capitalize">Our School at a Glance.</h2>
       <p className='px-4 text-center text-brand md:text-xl'>We have made incredible impacts and achieved sucess over the years. </p>
       <Impact/>
+      <h2 className="text-2xl md:text-3xl font-bold text-primary text-center w-full p-4 capitalize">Meet Our Team</h2>
+      <Tutors/>
 <Footer/>
    
     </div>
