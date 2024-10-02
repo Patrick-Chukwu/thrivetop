486be1d090adce7f164961188e8d3fcf4a4f9564
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-10-02 17:25:08 +0100

    feat: update account details

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,7 +6,19 @@ const Contact = () => {
     <div className='mt-24'>
     <Navbar/>
         <section className="flex flex-col p-4">
-            
+            <h2 className='text-center font-semibold text-3xl'>For fees payment:</h2>
+            <div className="flex flex-col gap-4">
+              <div className="flex justify-between">
+                <p className="font-bold">Bank: </p> <p className="font-medium">Ecobank</p>
+              </div>
+              <div className="flex justify-between">
+                <p className="font-bold">Account Name: </p> <p className="font-medium">Thrivetop Academy Ltd.</p>
+              </div>
+              <div className="flex justify-between">
+                <p className="font-bold">Account Number: </p> <p className="font-medium">3880120663</p>
+              </div>
+            </div>
+
         </section>
     </div>
   )
